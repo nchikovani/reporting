b5a96394e38d86ba5f15c597892fce243a18fe1e
Nikolay Chikovani <nchikovany1@gmail.com>
2020-07-09 21:52:47 +0300

    init, setup finished

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+function App() {
+    return(
+        <div>helloWorld</div>
+    );
+}
+export default App;
